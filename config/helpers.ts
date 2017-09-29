339aa18dec6136a54cee028b358413a0bb13cf6e
Sebastian Döll <sebastian@pixelmilk.com>
2017-09-29 23:54:25 +0200

    ~ fixing tserror

--- config/helpers.ts
+++ config/helpers.ts
@@ -1,7 +1,3 @@
 import * as Path from "path";
 
 export const root = Path.join.bind(Path, Path.resolve(__dirname, ".."));
-
-export const setupTests = global.requestAnimationFrame = (cb) => {
-    setTimeout(cb, 0)
-} 
--- src/app.spec.tsx
+++ src/app.spec.tsx
@@ -1,12 +1,12 @@
-import App from "./app";
-import React from "react";
-import { mount, configure } from "enzyme";
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import App from './app';
 
 configure({ adapter: new Adapter() });
 
-test("App renders the welcome test inside", () => {
+test('App renders the welcome test inside', () => {
   const wrapper = mount(<App />);
-  const div = wrapper.find("div");
-  expect(div.text()).toBe("Welcome Electron + React Adventurer");
+  const div = wrapper.find('div');
+  expect(div.text()).toBe('Welcome Electron + React Adventurer');
 });
